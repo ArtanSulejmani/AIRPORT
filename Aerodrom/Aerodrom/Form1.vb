17c1b546f9a4ad83ac0abb4ec1c9329dec1bbb78
Artan Sulejmani <49171718+ArtanSulejmani@users.noreply.github.com>
2022-08-24 16:25:27 +0200

    Add files via upload

---
+++ Aerodrom/Aerodrom/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+
+
+Namespace Aerodrom
+
+    Public Class Form1
+
+        Public Property Name As String
+        Public Property City As String
+        Public Property Short As String
+        Public Destinations As List(Of Destination)
+
+
+
+        Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+            Form2.Show()
+
+
+        End Sub
+
+        Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+            Form3.Show()
+        End Sub
+
+        Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
+            Console.WriteLine(Grad, "-", Ime, "-", Kratenka)
+
+
+        End Sub
+    End Class
---
+++ Aerodrom/Aerodrom/Form2.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+
+Namespace Aerodrom
+    Public Class Form2
+        Public Property Name As String
+        Public Property Distance As Integer
+        Public Property Price As Integer
+
+        Public Sub New(ByVal name As String, ByVal distance As Integer, ByVal price As Integer)
+            name = name
+            distance = distance
+            price = price
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return String.Format("{0}" & vbTab & "{1} km - {2} EUR", Name, Distance, Price)
+        End Function
+    End Class
+End Namespace
